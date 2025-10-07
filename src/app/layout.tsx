ce2ac785eea2ad6701284bffd3302f077b52625a
Altamish Mohammad <altamish6589@gmail.com>
2025-10-07 13:21:04 +0000

    Try fixing this error: `Console Error: Hydration failed because the serv

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -24,6 +24,7 @@ export default function RootLayout({
         className={cn(
           'min-h-screen bg-background font-body antialiased',
         )}
+        suppressHydrationWarning={true}
       >
         {children}
         <Toaster />
